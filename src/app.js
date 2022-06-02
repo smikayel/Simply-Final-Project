ce46f257eb42b929e9edb210d7cc87355644d4be
Hov1122 <avanyanhovhannes@gmail.com>
2022-06-02 19:03:59 +0400

    cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import logger from 'morgan'
 import fs from 'fs'
 import path from 'path'
+import cors from 'cors'
+import corsOptions from "../config/corsOptions.js";
 
 const app = express()
 
@@ -9,6 +11,8 @@ const env = process.env.NODE_ENV || 'development'
 
 const configStr = fs.readFileSync(path.resolve('src/config.json'), 'utf-8')
 const config = JSON.parse(configStr)[env]
+
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(logger('dev' /*, { skip: (req, res) => res.statusCode < 400 }*/))
---
+++ src/helpers/validations.js
@@ -0,0 +1,21 @@
+import { badRequestErrorCreator } from './errors.js'
+
+export const validateSchema = (schema) => {
+  if (typeof schema !== 'object' || schema === null) throw new Error('Schema is not an object')
+
+  return async (req, res, next) => {
+    const { params, body } = req
+
+    try {
+      schema.params && (await schema.params.validateAsync(params))
+      schema.body && (await schema.body.validateAsync(body))
+      return next()
+    } catch (error) {
+      next(badRequestErrorCreator(error.details))
+    }
+  }
+}
+
+export const validateJWT = (jwtSecret) => {
+  //TO DO: validate JWT
+}
